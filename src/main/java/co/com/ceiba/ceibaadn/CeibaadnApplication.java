c133280000a8e574bb751de1541ca08dd25ede65
jefry.londono <jefry.londono@JEFRY-LONDONO.ceiba.local>
2019-02-07 11:54:54 -0500

    origin

---
+++ src/main/java/co/com/ceiba/ceibaadn/CeibaadnApplication.java
@@ -0,0 +1,14 @@
+package co.com.ceiba.ceibaadn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeibaadnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeibaadnApplication.class, args);
+	}
+
+}
+
