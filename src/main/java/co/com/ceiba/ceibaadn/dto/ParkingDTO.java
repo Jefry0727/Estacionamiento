422b22e68a1c3cba3a9b7838c4b1539e239fe3f1
jefry.londono <jefry.londono@JEFRY-LONDONO.ceiba.local>
2019-02-13 09:07:48 -0500

    test

--- src/main/java/co/com/ceiba/ceibaadn/dto/ParkingDTO.java
+++ src/main/java/co/com/ceiba/ceibaadn/dto/ParkingDTO.java
@@ -3,8 +3,6 @@ package co.com.ceiba.ceibaadn.dto;
 import java.io.Serializable;
 import java.util.Date;
 
-import co.com.ceiba.ceibaadn.model.Vehicle;
-
 public class ParkingDTO implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
